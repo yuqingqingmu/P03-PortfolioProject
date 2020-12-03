7d018ac05e707a307b2a16f0b2f3fb0dfb3b53af
myqq <haurka217@gmail.com>
2020-12-03 22:30:05 +0800

    Final

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,8 @@
-function openNav() {
-  document.getElementById(
-    "myNav").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById(
-    "myNav").style.width = "0%";
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
